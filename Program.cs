1515cc94d0e73b4e6527a48c16df3aa832073e79
mywatt <padamu1@naver.com>
2022-09-12 02:51:14 +0900

    프로그램 내용 수정

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ public class Program
         bool isServerStop = false;
         while (!isServerStop)
         {
-            Console.Write("Server For RockScissorPaper\n");
+            Console.Write("Server start\n");
             Console.WriteLine("서버를 종료하시려면 \"quit\"를 입력해주세요.");
             switch (Console.ReadLine())
             {
